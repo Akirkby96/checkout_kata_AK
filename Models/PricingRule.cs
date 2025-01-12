b0f725a58e315fa2aed7a001b4517e7977822dd1
AK <aidan.j.kirkby@hotmail.com>
2025-01-12 15:10:22 +0000

    Adding Constructors and Dictionarys

--- Models/PricingRule.cs
+++ Models/PricingRule.cs
@@ -1,9 +1,15 @@
 ﻿namespace Checkout.Kata.Models
 {
-    public class PricingRule
+    /// <summary>
+    /// Class to hold pricing rules for items
+    /// </summary>
+    /// <param name="unitPrice"></param>
+    /// <param name="specialQuantity"></param>
+    /// <param name="specialPrice"></param>
+    public class PricingRule(int unitPrice, int specialQuantity = 0, int specialPrice = 0)
     {
-        public int UnitPrice { get; set; }
-        public int SpecialQuantity { get; set; }
-        public int SpecialPrice { get; set; }
+        public int UnitPrice { get; set; } = unitPrice;
+        public int SpecialQuantity { get; set; } = specialQuantity;
+        public int SpecialPrice { get; set; } = specialPrice;
     }
 }
--- Services/CheckoutKata.cs
+++ Services/CheckoutKata.cs
@@ -1,9 +1,29 @@
-﻿using Checkout.Kata.Services.Interfaces;
+﻿using Checkout.Kata.Models;
+using Checkout.Kata.Services.Interfaces;
 
 namespace Checkout.Kata.Services
 {
     public class CheckoutKata : ICheckoutKata
     {
+
+        // Stores the pricing rules for each unique item
+        private readonly Dictionary<string, PricingRule> _pricingRules;
+
+        // Stores how many of each item has been scanned
+        private readonly Dictionary<string, int> _scannedItems;
+
+        /// <summary>
+        /// Constructor for CheckoutKata
+        /// </summary>
+        /// <param name="pricingRules"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public CheckoutKata(Dictionary<string, PricingRule> pricingRules)
+        {
+            _pricingRules = pricingRules;
+            _scannedItems = new Dictionary<string, int>();
+        }
+
+
         /// <summary>
         /// 
         /// </summary>
@@ -22,5 +42,7 @@ namespace Checkout.Kata.Services
         {
             throw new System.NotImplementedException();
         }
+
+
     }
 }
